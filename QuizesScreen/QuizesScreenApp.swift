8251227c6f39ccae9b2536b1fb91924f98bcc131
Artem <timmi.gri@gmail.com>
2023-01-04 21:21:01 +0300

    Initial Commit

---
+++ QuizesScreen/QuizesScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizesScreenApp.swift
+//  QuizesScreen
+//
+//  Created by Артём Грищенко on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizesScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
